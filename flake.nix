407b8a6159668c148e8c24c90530aadff617dfd3
Wojtek Czekalski <me@wczekalski.com>
2026-08-11 20:48:36 +0200

    update dnvr

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    dnvr.url = "github:adamchol/dnvr/postgres-as-unprivileged";
+    dnvr.url = "github:dialohq/dnvr";
   };
 
   outputs =
